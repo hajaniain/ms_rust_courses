df42d73c7e9f113da97b4b7be825744bb451b570
hajaniain <hajaniainamarius@gmail.com>
2021-08-18 07:55:51 +0200

    next

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+struct Car {
+    make: String,
+    model: String,
+    year: u32,
+}
+
+struct Point2D(u32, u32);
+
 fn main() {
     let x = 0.5;
     println!("x: {}", x);
@@ -11,10 +19,28 @@ fn main() {
     println!("Info about => {} is {} years old", tuple.0, tuple.1);
 
     println!("sum of [2, 4 ,6] = {}", array_sum([2, 4, 6]));
+
+    let car = Car {
+        make: String::from("Honda"),
+        model: String::from("Accord"),
+        year: 2015,
+    };
+
+    println!("car make: {}", car.make);
+
+    let origin = Point2D(100, 200);
+
+    println!("Point contains {:?} and {:?}", origin.0, origin.1);
+
+    let Point2D(x, y) = origin;
+    println!("Point contains {:?} and {:?}", x, y);
+
 }
 
 fn array_sum(array: [i32; 3]) -> i32 {
     array.iter().sum()
 }
+
+
 
 
