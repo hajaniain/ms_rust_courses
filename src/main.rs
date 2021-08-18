c0c58ff7f546a143226c60028b6a364cb9f01499
hajaniain <hajaniainamarius@gmail.com>
2021-08-18 19:36:20 +0200

    conditions

--- src/main.rs
+++ src/main.rs
@@ -62,9 +62,44 @@ fn main() {
 
     println!("Person name is: {}", person.name);
 
+    // ENUMS
+
     let quit = WebEvent::Keypress('q');
 
     let something = Some(1);
+
+    // CONDITIONS
+
+    if 1 == 2 {
+        println!("math is broken")
+    } else {
+        println!("everything is fine");
+    }
+
+    let formal = true;
+    let greeting = if formal {
+        println!("Good evening");
+    } else {
+        println!("Hey, friend");
+    };
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else {
+        println!("number is not divisible by 4 or 3");
+    };
+
+    let boolean = true;
+
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    };
+
 }
 
 fn array_sum(array: [i32; 3]) -> i32 {
