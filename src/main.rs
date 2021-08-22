ba8e5ec7c2818d1a4fdbe93c56c96a0208f5657d
hajaniain <hajaniainamarius@gmail.com>
2021-08-22 19:49:02 +0200

    last

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 struct Car {
     make: String,
     model: String,
@@ -25,6 +27,11 @@ enum Option<T> {
     None,
 }
 
+
+#[derive(PartialEq, Eq)]
+struct Student {
+    name: String,
+}
 fn main() {
     let x = 0.5;
     println!("x: {}", x);
@@ -124,12 +131,87 @@ fn main() {
     }
 
     // Error handler
+
+    // panic!("Farewell!!")
+    // let v = vec![0, 1, 2, 3];
+
+    // println!("{}", v[6]);
+
+    // let fruits = vec!["banana", "apple", "coconut"];
+
+    // let first = fruits.get(0);
+
+    // println!("{:?}", first);
+
+    // let non_existent = fruits.get(99);
+    // println!("{:?}", non_existent);
+
+    // let f = File::open("hello.txt");
+
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Can't open the file {:?}", error),
+    // };
+
+    // OWNERSHIP / BORROWING
+
+    let say = String::from("Cat");
+    // print_out(say);
+    print_out(&say);
+
+    println!("Again {}", say);
+
+    let mut my_vec = vec![1, 2, 3];
+    println!("{:?}", my_vec);
+
+    add_to_vec(&mut my_vec);
+    println!("{:?}", my_vec);
+
+    // STRING
+    let text = "Hello\nworld\n!";
+    let upper = text.to_uppercase();
+    let stripped = upper.strip_prefix("HELLO\n").unwrap();
+    println!("{}", first_line(stripped));
+
+    // COLLECTIONS
+    let mut students = vec![Student {
+        name: "Ryan".to_string()
+    }];
+    students.push(Student {
+        name: "Lisa".to_string(),
+    });
+
+    assert!(&students[0] == &Student {name: "Ryan".to_string()});
+
+    assert!(students.get(0) == Some(&Student {name: "Ryan".to_string()}));
+
+    assert!(students.get(1000) == None);
+
+    for student in students.iter() {
+        println!("Student name: {}", student.name);
+    }
+
+    use std::collections::HashMap;
+
+    let mut enrollment = HashMap::new();
+    enrollment.insert("biology".to_string(), students);
+
+    let bio_students = enrollment.get("biology");
+    let students = enrollment.remove("biology");
 }
 
 fn array_sum(array: [i32; 3]) -> i32 {
     array.iter().sum()
 }
 
+fn print_out(to_print: &String) {
+    println!("{}", to_print);
+}
 
+fn add_to_vec(a_vec: &mut Vec<i32>) {
+    a_vec.push(4);
+}
 
-
+pub fn first_line(string: &str) -> &str {
+    string.lines().next().unwrap()
+}
